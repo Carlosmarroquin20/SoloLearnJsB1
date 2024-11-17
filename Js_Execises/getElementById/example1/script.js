46004b1e5fff22c324e735337f9c3f5932057678
Emanuel Marroquin <carlosema322@gmail.com>
2024-11-16 22:11:06 -0600

    Ejemplo con getElementById

---
+++ Js_Execises/getElementById/example1/script.js
@@ -0,0 +1,10 @@
+function cambiarTexto(){
+    var elemento = document.getElementById("texto");
+
+    elemento.innerHTML = "Texto cambiado correctamente";
+};
+
+function cambiarColor(){
+    var elemento = document.getElementById("texto");
+    elemento.style.color = "red";
+};

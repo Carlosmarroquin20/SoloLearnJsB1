51f8d6c0c1e9dbc9441b1553d5ba05102c918912
Emanuel Marroquin <carlosema322@gmail.com>
2024-11-18 12:47:36 -0600

    Ejemplos de if else y if else

---
+++ Js_Execises/EstructurasEasy/If.js
@@ -0,0 +1,7 @@
+var numero = 5;
+
+if (numero > 5) {
+    console.log("El numero es mayor a 5")
+} else {
+    console.log("El numero es 5 o es menor a 5")
+}
---
+++ Js_Execises/EstructurasEasy/else_if1.js
@@ -0,0 +1,9 @@
+var edad = 2;
+
+if (edad < 13) {
+    console.log("Eres un nino");
+} else if (edad >= 13 && edad <18) {
+    console.log("Eres un adolecente")
+} else {
+    console.log("Eres un adulto")
+}
